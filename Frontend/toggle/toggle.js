64f80299a43a8ead5441d0bd3997b2005e1e3f9c
soumik7063 <ghataksoumik7063@gmail.com>
2025-03-05 23:49:09 +0530

    hamburger bug fixed

--- Frontend/toggle/toggle.js
+++ Frontend/toggle/toggle.js
@@ -1,10 +1,11 @@
 // Toggle the navbar visibility on small screens
-document.getElementById("menu-toggle").addEventListener("click", function () {
-  const navbar = document.querySelector(".navbar");
-
-  // Toggle the "active" class to show/hide the menu
-  navbar.classList.toggle("active");
-
-  // Optionally, prevent body scroll when the menu is open
-  document.body.classList.toggle("menu-open");
+let isMenuOpen = false;
+document.getElementById("menu-toggle").addEventListener("click",  ()=> {
+  if(!isMenuOpen){
+    document.querySelector(".navbar").classList.add("active");
+    isMenuOpen = true;
+  } else {
+    document.querySelector(".navbar").classList.remove("active");
+    isMenuOpen = false;
+  }
 });
